aa3938f24e88faad85f13bb9f8fa89743d48e539
Sarmiento <Didier.Sarmiento.consultant@nielsen.com>
2018-11-12 23:32:26 -0600

    Agregando los valores en el application.properties y bootstrap

--- src/main/java/com/springboot/thymeleaf/app/controllers/IndexController.java
+++ src/main/java/com/springboot/thymeleaf/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.springboot.thymeleaf.app.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 	
+	@Value("${application.controllers.message}")
+	private String message;
+	
 	@GetMapping("/") // Si es una petición GET
 	public String greeting(Model model) {
-		model.addAttribute("message", "Hello World from Spring Framework and Thymeleaf");
+		model.addAttribute("message", message);
 		return "hello";
 	}
 }
